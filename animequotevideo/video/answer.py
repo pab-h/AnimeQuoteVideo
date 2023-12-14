343c1bfa5e7712481e615aee5aca9eab017ad992
pab-h <dev.pab.2020@gmail.com>
2023-12-13 21:42:54 -0300

    feat: Video de resposta

---
+++ animequotevideo/video/answer.py
@@ -0,0 +1,46 @@
+from numpy import array
+
+from animequotevideo.models.quote import Quote
+from animequotevideo.video.default import Default
+
+from PIL import Image
+from PIL import ImageFilter
+
+from moviepy.editor import ImageClip
+from moviepy.editor import TextClip
+from moviepy.editor import CompositeVideoClip
+from moviepy.editor import VideoClip
+
+class Answer(Default):
+    def __init__(self, quote: Quote) -> None:
+        super().__init__(quote)
+
+    def build(self) -> VideoClip:
+        with self.images as images: 
+            images.getCharacter(self.quote)
+            images.getAnimeWallpaper(self.quote)
+
+        background = Image.open(self.quote.animeImage)
+        background = background.resize((1280, 720))
+        background = background.filter(ImageFilter.BoxBlur(10)) 
+        background = ImageClip(array(background))
+
+        photo = Image.open(self.quote.characterImage)
+        photo = photo.resize((400, 400))
+        photo = ImageClip(array(photo))
+        photo = photo.set_position("center")
+
+        name = TextClip(
+            self.quote.character,
+            color = "white",
+            fontsize = 50,
+            kerning = 1
+        )
+        name = name.margin(bottom = 75, opacity = 0)
+        name = name.set_position("bottom")
+
+        video = CompositeVideoClip([ background, photo, name ])
+        video = video.set_duration(5)
+        video = video.set_fps(1)
+
+        return video
