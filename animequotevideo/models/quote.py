02693d46ce39a1e9064657e10d8d9b0ba5bed23e
pab-h <dev.pab.2020@gmail.com>
2023-12-13 21:42:09 -0300

    feat: atributos para armazenar as imagens

--- animequotevideo/models/quote.py
+++ animequotevideo/models/quote.py
@@ -1,11 +1,10 @@
-from dataclasses import dataclass
-from dataclasses import field
+from dataclasses import dataclass, field
 from typing import Optional
 
 @dataclass
 class Quote:
     anime: str
-    animeImage: Optional[str] = field(default = None)
     character: str
-    characterImage: Optional[str] = field(default = None)
     quote: str
+    animeImage: Optional[str] = field(default=None)
+    characterImage: Optional[str] = field(default=None)
