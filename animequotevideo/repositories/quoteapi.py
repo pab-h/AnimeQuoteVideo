09ea48f5b40cb1d429357bb78fda532ff19600f2
pab-h <dev.pab.2020@gmail.com>
2023-12-20 19:53:05 -0300

    feat: adaptacao para o cache

--- animequotevideo/repositories/quoteapi.py
+++ animequotevideo/repositories/quoteapi.py
@@ -1,6 +1,7 @@
 import requests
 
 from animequotevideo.models.quote import Quote
+from animequotevideo.repositories.quotecache import QuoteCache
 
 class QuoteApi:
     def __init__(self) -> None:
@@ -8,25 +9,45 @@ class QuoteApi:
 
     def randomQuote(self) -> Quote:
         response = requests.get(f"{self.url}/random")
-        data = response.json()
 
-        return Quote(
-            anime = data.get("anime"),
-            character = data.get("character"),
-            quote = data.get("quote"),
-        )
+        with QuoteCache() as cache: 
+            try:
+                data = response.json()
+
+                quote = Quote(
+                    anime = data.get("anime"),
+                    character = data.get("character"),
+                    quote = data.get("quote"),
+                )
+
+                if not cache.exitsQuote(quote):
+                    cache.store(quote)
+
+                return quote
+            except:
+                return cache.randomQuote()
 
     def randomQuotes(self) -> list[Quote]:
         response = requests.get(f"{self.url}/quotes")
-        data = response.json()
 
-        quotes = []
+        with QuoteCache() as cache: 
+            try:
+                data = response.json()
 
-        for quote in data:
-            quotes.append(Quote(
-                anime = quote.get("anime"),
-                character = quote.get("character"),
-                quote = quote.get("quote"),
-            ))
+                quotes = []
 
-        return quotes
+                for quoteData in data:
+                    quote = Quote(
+                        anime = quoteData.get("anime"),
+                        character = quoteData.get("character"),
+                        quote = quoteData.get("quote"),
+                    )
+
+                    if not cache.exitsQuote(quote):
+                        cache.store(quote)
+
+                    quotes.append(quote)
+
+                return quotes
+            except:
+                return cache.randomQuotes()
